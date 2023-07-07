323d511f56443953d6e15d274a32ff32f900b33f
wingshope <138878860+wingshope@users.noreply.github.com>
2023-07-07 20:33:01 +0700

    Update speedtestfix.sh

--- tools/speedtestfix.sh
+++ tools/speedtestfix.sh
@@ -7,7 +7,7 @@ cd
 rm -rf /usr/bin/speedtest
 echo -e "\e[1;32m Start download speedtest.. \e[0m"
 cd /usr/bin
-wget -O speedtest "https://raw.githubusercontent.com/wingshope10/Agoez120/master/fix/speedtest"
+wget -O speedtest "https://raw.githubusercontent.com/wingshope/agoez/master/fix/speedtest"
 chmod +x speedtest
 clear
 echo -e "\e[1;32m Setup done Please wait.. \e[0m"
